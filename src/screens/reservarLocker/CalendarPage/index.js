587e632f52922f522f3d2fe999b71c74abf3ba83
clydeskoya <joao.atalho@protonmail.com>
2021-03-01 16:51:20 +0000

    libs calendar e elements adicionadas

--- src/screens/reservarLocker/CalendarPage/index.js
+++ src/screens/reservarLocker/CalendarPage/index.js
@@ -1,15 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+
 import CalendarPageRender from "./CalendarPageRender";
 
 class CalendarPage extends React.Component {
-
-
   render() {
     return <CalendarPageRender />;
   }
-
-  
 }
 
 export default CalendarPage;
