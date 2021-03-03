8059bf275195229fcf5f71f8da9186de8d904cf7
clydeskoya <joao.atalho@protonmail.com>
2021-03-03 21:25:23 +0000

    primeiro ecrã do ReserveScreen já aparece

--- src/screens/ReserveScreen.js
+++ src/screens/ReserveScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { Header } from "../components/Header";
-import { Calendar } from "./reservarLocker/CalendarPage/index";
+import Calendar from "./reservarLocker/CalendarPage/index";
 export default function Reserve() {
   return (
     <View style={styles.container}>
