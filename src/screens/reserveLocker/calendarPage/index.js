1d0701094176b852a1b5afb7d3694c3c0cb8f963
Traça <ggpt98@gmail.com>
2021-03-03 21:00:06 +0000

    calendar

---
+++ src/screens/reserveLocker/calendarPage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import CalendarPageRender from "./CalendarPageRender";
+
+class Calendar extends React.Component {
+  render() {
+    return <CalendarPageRender />;
+  }
+}
+
+export default Calendar;
