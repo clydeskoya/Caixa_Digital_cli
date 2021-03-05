f3f5015d4d730398ad8ddadd4e206cf47313bd3d
Ben-Hur Fidalgo <ben.hur.fidalgo@jmellosaude.pt>
2021-03-05 01:42:54 +0000

    + Wrap up eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,46 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
+    es6: true,
   },
   extends: [
-    'plugin:react/recommended',
     'airbnb',
+    'prettier',
+    'prettier/react',
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
   plugins: [
     'react',
+    'prettier',
   ],
   rules: {
-    'import/prefer-default-export': 0,
-    // 'linebreak-style': ['error', 'windows'],
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'off',
   },
 };
+// https://edusutil.medium.com/eslint-with-prettier-settings-for-react-native-ce13d2aaf500
+// https://www.imaginarycloud.com/blog/how-to-configure-eslint-prettier-in-react/
+// https://www.andrewmin.info/blog/react-setup/
