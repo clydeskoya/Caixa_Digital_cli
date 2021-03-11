920820cb0c50bc9fc6081a7dcfa2b19af19856b2
Traça <ggpt98@gmail.com>
2021-03-11 00:12:53 +0000

    send receive

--- src/screens/reservarLocker/SendOrReceive/SendReceivePageRender.js
+++ src/screens/reservarLocker/SendOrReceive/SendReceivePageRender.js
@@ -2,28 +2,58 @@ import React from 'react';
 import { MaskedViewComponent, StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import Header from '../../../components/HeaderReservarLocker';
+import { RadioButton } from 'react-native-paper';
 
 const SendReceivePage = () => {
+  const [checked, setChecked] = React.useState('first');
   return (
     <View>
       <Header />
-
-      <Text>Send or Receive</Text>
-
-      <TouchableOpacity
-        onPress={() => {
-          Actions.payment();
-        }}
-      >
-        <View style={styles.button}>
-          <Text style={styles.textr}>Seguinte</Text>
+      <View style={styles.maindiv}>
+        <Text style={styles.Simpletext}>Que finalidade da reserva?</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', marginLeft: 20, marginTop: 10 }}>
+          <RadioButton
+            value="first"
+            status={checked === 'first' ? 'checked' : 'unchecked'}
+            onPress={() => setChecked('first')}
+          />
+          <View style={styles.radiotext}>
+            <Text style={styles.Simpletext}>Envio de correspondências</Text>
+          </View>
         </View>
-      </TouchableOpacity>
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-start', marginLeft: 20 }}>
+          <RadioButton
+            value="second"
+            status={checked === 'second' ? 'checked' : 'unchecked'}
+            onPress={() => setChecked('second')}
+          />
+          <View style={styles.radiotext}>
+            <Text style={styles.Simpletext}>Recebimento de correspondências</Text>
+          </View>
+        </View>
+
+        <TouchableOpacity
+          onPress={() => {
+            Actions.payment();
+          }}
+        >
+          <View style={styles.button}>
+            <Text style={styles.textr}>Seguinte</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  maindiv: {
+    marginTop: 75,
+    marginLeft: 20,
+  },
+  Simpletext: {
+    fontSize: 16,
+  },
   button: {
     alignItems: 'center',
     width: 290,
@@ -38,6 +68,9 @@ const styles = StyleSheet.create({
     color: 'white',
     marginVertical: 5,
     fontSize: 22,
+  },
+  radiotext: {
+    marginVertical: 5,
   },
 });
 
