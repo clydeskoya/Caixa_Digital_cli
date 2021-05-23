6aad00d0b568e29cc6ad56e81fbbb7c7c040874a
Traça <ggpt98@gmail.com>
2021-05-23 02:19:20 +0100

    ScanQRCode

--- src/components/ScanQrCode.js
+++ src/components/ScanQrCode.js
@@ -3,49 +3,19 @@ import React, { useState, useEffect, useContext } from 'react';
 import { Image, Text, View, StyleSheet, Button, Dimensions } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import Constants from 'expo-constants';
-import { useNavigation } from '@react-navigation/core';
+
 import base64 from 'react-native-base64';
 import { prop } from 'ramda';
 import { LoginContext } from '../common/loginHelper/responseData';
+import axios from 'axios';
 
 const { width } = Dimensions.get('window');
 const qrSize = width * 0.7;
 const REGEX_CODE = /[A-Za-z0-9+/=]/;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-  qr: {
-    marginTop: '20%',
-    marginBottom: '20%',
-    width: qrSize,
-    height: qrSize,
-  },
-  description: {
-    fontSize: width * 0.09,
-    marginTop: '10%',
-    textAlign: 'center',
-    width: '70%',
-    color: 'white',
-  },
-  cancel: {
-    fontSize: width * 0.05,
-    textAlign: 'center',
-    width: '70%',
-    color: 'white',
-  },
-});
-
-export default function App() {
+const ScanQrCode = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  const navigation = useNavigation();
 
   useEffect(() => {
     (async () => {
@@ -60,20 +30,30 @@ export default function App() {
       if (REGEX_CODE.test(data)) {
         const dcode = base64.decode(data);
         const dataParsed = JSON.parse(dcode);
-        const postalCodeLogin = loginContext.loginData.user.entity.address.postalCode;
+       
+        const  {data1}  = await axios.post(`${API_URL}/orders/sendPackage`, {
+          id: props.route.params.id
+        });
+        console.log(data1);
+        setScanned(true);
+        //const postalCodeLogin = loginContext.loginData.user.entity.address.postalCode;
         const postalCodeParsed = prop('postalCode', dataParsed);
-        if (postalCodeParsed === postalCodeLogin) {
-          setScanned(true);
+        // if (postalCodeParsed === postalCodeLogin) {
+        //   setScanned(true);
+        
+         
+        // } else {
+        //   setScanned(true);
+        //   alert('O locker não está associado à sua conta!');
+       
 
-          navigation.navigate('ScanSuccess');
-        } else {
-          setScanned(true);
-          alert('O locker não está associado à sua conta!');
-        }
+        // }
       }
     } catch (error) {
       alert('O código QR não está associado a nenhum locker');
       console.log('erro do catch', error);
+      // console.log(postalCodeLogin);
+      console.log(postalCodeParsed);
     }
 
     if (hasPermission === null) {
@@ -105,4 +85,36 @@ export default function App() {
       {scanned && <Button title="Tap to Scan Again" onPress={() => setScanned(false)} />}
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  qr: {
+    marginTop: '20%',
+    marginBottom: '20%',
+    width: qrSize,
+    height: qrSize,
+  },
+  description: {
+    fontSize: width * 0.09,
+    marginTop: '10%',
+    textAlign: 'center',
+    width: '70%',
+    color: 'white',
+  },
+  cancel: {
+    fontSize: width * 0.05,
+    textAlign: 'center',
+    width: '70%',
+    color: 'white',
+  },
+});
+
+export default ScanQrCode;
